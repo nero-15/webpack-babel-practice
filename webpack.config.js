f4324fb0039fb12723aea47a0c314fd32f7a7922
nero-15 <ti7805@gmail.com>
2021-02-28 15:44:12 +0900

    setting webpack config  watch

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
 module.exports = {
 	mode: 'development',
+	devtool: 'inline-source-map',
+	watch: true,
+	watchOptions: {
+		ignored: /node_modules/
+	},
 	entry: './src/index.js',
 	output: {
 		path: __dirname + '/dist',
