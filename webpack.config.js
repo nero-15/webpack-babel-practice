713bb9c4762eaa6a0f6e0132c8a3183f12bdde30
nero-15 <ti7805@gmail.com>
2021-02-28 16:23:05 +0900

    setting webpack and babel

--- webpack.config.js
+++ webpack.config.js
@@ -9,5 +9,19 @@ module.exports = {
 	output: {
 		path: __dirname + '/dist',
 		filename: 'main.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
+			}
+		]
 	}
 };
